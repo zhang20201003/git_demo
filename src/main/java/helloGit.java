767dbf4834b3aa0c250221c4bc8b124b8ad74db0
zhang <1836591545@qq.com>
2022-05-05 20:01:02 +0800

    fifth version master

--- src/main/java/helloGit.java
+++ src/main/java/helloGit.java
@@ -4,5 +4,6 @@ public class helloGit {
         System.out.println("hello git2!");
         System.out.println("hello git3!");
         System.out.println("hello git4!");
+        System.out.println("hello git5 master!");
     }
 }
